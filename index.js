c794b8ac85807edb2f244ae49201dc1224a8b814
NguyenTanCACA <nguyentanalat@gmail.com>
2025-08-07 19:03:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ require("dotenv").config();
 const app = express();
 
 // ✅ THÊM CORS HEADER TẠI ĐÂY
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Hoặc ghi rõ tên miền cụ thể thay vì '*'
-  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(204);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // Hoặc ghi rõ tên miền cụ thể thay vì '*'
+//   res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") return res.sendStatus(204);
+//   next();
+// });
+app.use(cors());
 
 app.use(express.json());
 
